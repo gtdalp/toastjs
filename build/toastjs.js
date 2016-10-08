5d3c8ea2142ff8e001f1bfebddceb37059c17457
gtdalp <295194117@qq.com>
2016-10-08 22:17:31 +0800

    update

--- build/toastjs.js
+++ build/toastjs.js
@@ -8,7 +8,6 @@
     // toastjs组件
     window.ToastJs = function (val, time, callback) {
         var options = {
-            id: 'toastjs',
             val: val || '',
             time: time || 3000,
             callback: callback
@@ -25,18 +24,35 @@
         init: function () {
             this.render();
         },
+        isFunction: function( fn ) {
+          return  !!fn &&                  // 不为空，存在
+            !fn.nodeName &&                // 不是节点对象
+            fn.constructor != String &&    // 不是字符串类型
+            fn.constructor != RegExp &&    // 不是正则表达式
+            fn.constructor != Array &&     // 不是数组
+            /function/i.test( fn + "" );   // toString()中包含"function"
+        },
         // 渲染
         render: function () {
             var op = this.options;
+            var doc = document;
+            op.id = 'toastjs';
             window.onload = function () {
-                var toastjs = document.getElementById(op.id);
+                var toastjs = doc.getElementById(op.id);
                 if (!toastjs) {
-                    var body = document.querySelector('body');
-                    var toastjsNode = document.createElement("div");
-                    toastjsNode.innerHTML = op.val;
-                    body.appendChild(toastjsNode);
+                    var body = doc.body;
+                    var div = doc.createElement("div");
+                    div.innerHTML = op.val;
+                    div.id = op.id;
+                    body.appendChild(div);
+                    div.className = 'toastjs';
+                    var width = div.clientWidth/2;
+                    var height = div.clientHeight/2;
+                    div.setAttribute('style', "margin:-" + height + "px 0 0 -" + width + "px;top:50%;");
+                    div.className = 'toastjs toastjs-animate-in';
                 }
-            }
+                this.hide();
+            }.bind(this);
         },
         // 显示
         show: function () {
@@ -44,11 +60,33 @@
         },
         // 隐藏
         hide: function () {
-            //
+            var op = this.options;
+            var t = op.time;
+            var cb = this.isFunction(op.callback) ? op.callback : '';
+            var toastjs = document.getElementById(op.id);
+            if (toastjs) {
+                setTimeout(function () {
+                    var cls = toastjs.getAttribute('class') + ' toastjs-animate-out';
+                    toastjs.setAttribute('class', cls);
+                    // 删除dom节点和回调
+                    setTimeout(function () {
+                        toastjs.remove();
+                        // 回调
+                        if (cb) {
+                            cb();
+                        }
+                    }, 806);
+                    
+                }, t);
+                
+            }
         },
         // 销毁
         destroy: function () {
-            //
+            var toastjs = document.getElementById(op.id);
+            if (toastjs) {
+                toastjs.remove();
+            }
         }
     }
 })();
